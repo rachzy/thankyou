fe8f50e87e15a72419fcbecb77621f31441c76ab
rachzy <henriqueferreira.fox@gmail.com>
2024-12-31 17:45:48 -0300

    create initial files

---
+++ index.js
@@ -0,0 +1,34 @@
+const loadingText = document.querySelector(".loading-text");
+const loadingContainer = document.querySelector(".loading-container");
+const container = document.querySelector(".container");
+
+const allImages = document.querySelectorAll("img");
+
+const loadingLabel = "Carregando";
+let amountOfDots = 1;
+
+const imagesAsPromises = Array.from(allImages).map((image) => {
+  return new Promise((resolve) => {
+    image.addEventListener("load", () => {
+      resolve(image);
+    });
+  });
+});
+
+const loadingAnimation = setInterval(() => {
+  amountOfDots++;
+  if (amountOfDots > 3) {
+    amountOfDots = 1;
+  }
+  loadingText.textContent = loadingLabel + ".".repeat(amountOfDots);
+}, 250);
+
+function displayContent() {
+  clearInterval(loadingAnimation);
+  loadingContainer.style.display = "none";
+}
+
+Promise.all(imagesAsPromises).then(() => {
+  clearInterval(loadingAnimation);
+  displayContent();
+});
